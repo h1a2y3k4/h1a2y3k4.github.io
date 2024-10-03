e3aa6d369acb08d931f8e5c33af4229c9a432971
Hayk Karapetyan <158153949+h1a2y3k4@users.noreply.github.com>
2024-10-03 09:18:21 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+try {
+  const data = fs.readFileSync('./data.json', 'utf8');
+  console.log('File content:', data);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
+
