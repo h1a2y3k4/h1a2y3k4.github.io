23568eb49a3fec9198bda84317c9ac41207eed0f
Hayk Karapetyan <158153949+h1a2y3k4@users.noreply.github.com>
2024-10-03 10:03:13 +0400

    Create js.mjs

---
+++ js.mjs
@@ -0,0 +1,8 @@
+console.log("hhhhhhhh")
+const fs = require('fs');
+try {
+  const data = fs.readFileSync('./data.json', 'utf8');
+  console.log('File content:', data);
+} catch (err) {
+  console.error('Error reading file:', err);
+}
