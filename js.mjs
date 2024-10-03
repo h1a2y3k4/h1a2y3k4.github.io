6e6f702ef5aff7d17400562edafeea364cac04d2
Hayk Karapetyan <158153949+h1a2y3k4@users.noreply.github.com>
2024-10-03 10:26:09 +0400

    Update js.mjs

--- js.mjs
+++ js.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs';
 try {
-  const data = fs.readFileSync('./data.json', 'utf8');
+  const data = fs.readFileSync('https://h1a2y3k4.github.io/data.json', 'utf8');
   console.log('File content:', data);
 } catch (err) {
   console.error('Error reading file:', err);
