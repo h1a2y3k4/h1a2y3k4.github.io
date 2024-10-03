7bd7de6d11185406ab78ff4a65f5a3ad0661cadf
Hayk Karapetyan <158153949+h1a2y3k4@users.noreply.github.com>
2024-10-03 10:02:56 +0400

    Delete js.js

--- js.js
+++
@@ -1,9 +0,0 @@
-console.log("hhhhhhhh")
-const fs = require('fs');
-try {
-  const data = fs.readFileSync('./data.json', 'utf8');
-  console.log('File content:', data);
-} catch (err) {
-  console.error('Error reading file:', err);
-}
-
